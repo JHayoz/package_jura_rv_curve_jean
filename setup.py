8af4fc458d5282a5cf6c97118d8958d6247d4b9c
JHayoz <jeanhayoz94@gmail.com>
2023-05-09 12:10:14 +0200

    added description for pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import setuptools
 
+#Let us store all the info inside the readme file in the
+#long description variable. This description will be shown in test.pypi webiste
+with open("README.md",'r') as fh:
+    long_description=fh.read()
+
 setuptools.setup(
     name='rv_curve_jura',
     version='0.1',
@@ -7,6 +12,9 @@ setuptools.setup(
     author_email='jeanhayoz94@gmail.com',
     description='Plot all RV curves that you want!',
     packages=setuptools.find_packages(),
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    url='git@github.com:JHayoz/package_jura_rv_curve_jean',
     install_requires=['numpy','matplotlib'],
     classifiers=['Programming Language :: Python :: 3'],
 )
